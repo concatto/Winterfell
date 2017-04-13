9703c19a0790f84865aaa0c3a70ca341d3ed9931
Fernando Concatto <fernandoconcatto@gmail.com>
2017-04-13 03:39:18 -0300

    Progresso na adaptação para React

---
+++ React/src/publication-data.js
@@ -0,0 +1,36 @@
+export const DATA = [
+	{
+		author: {
+			avatar: "assets/avatar.jpg",
+			name: "Samuel Favarin"
+		},
+		timestamp: 1492061255,
+		content: {
+			title: "Prova de que o React funciona",
+			image: "assets/react.png"
+		},
+		reactions: 10
+	}, {
+		author: {
+			avatar: "assets/avatar.jpg",
+			name: "Vinicius Almeida"
+		},
+		timestamp: 1492060255,
+		content: {
+			title: "Prova de que o React funciona",
+			image: "assets/react.png"
+		},
+		reactions: 30
+	}, {
+		author: {
+			avatar: "assets/avatar.jpg",
+			name: "Miguel Copatti"
+		},
+		timestamp: 1492058255,
+		content: {
+			title: "Prova de que o React funciona",
+			image: "assets/react.png"
+		},
+		reactions: 40
+	}
+];
