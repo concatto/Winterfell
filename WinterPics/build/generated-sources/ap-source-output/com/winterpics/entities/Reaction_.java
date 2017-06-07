6d869711d140491f98c1e456c2e261d216be8b2f
Vinícius Almeida dos Santos <vinicius@rudinei.cnt.br>
2017-06-07 16:43:26 -0300

    Persistência alterada para servidor online

--- WinterPics/build/generated-sources/ap-source-output/com/winterpics/entities/Reaction_.java
+++ WinterPics/build/generated-sources/ap-source-output/com/winterpics/entities/Reaction_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-07T16:31:24")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-07T16:41:36")
 @StaticMetamodel(Reaction.class)
 public class Reaction_ { 
 
--- WinterPics/build/generated-sources/ap-source-output/com/winterpics/entities/WinterUser_.java
+++ WinterPics/build/generated-sources/ap-source-output/com/winterpics/entities/WinterUser_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-07T16:31:24")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-07T16:41:36")
 @StaticMetamodel(WinterUser.class)
 public class WinterUser_ { 
 
