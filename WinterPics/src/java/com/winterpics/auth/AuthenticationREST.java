24315db668c8730a078780d34df71d91100c420d
Vinícius Almeida dos Santos <vinicius@rudinei.cnt.br>
2017-06-12 20:02:59 -0300

    Alteração de cadastro de usuário

--- WinterPics/src/java/com/winterpics/auth/AuthenticationREST.java
+++ WinterPics/src/java/com/winterpics/auth/AuthenticationREST.java
@@ -6,24 +6,27 @@ import javax.persistence.EntityManager;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("auth")
+@Path("newuser")
 public class AuthenticationREST {
 
 //    private final UserAuthenticator userAuthenticator = new UserAuthenticator();
     
     @POST
     @Consumes({MediaType.APPLICATION_JSON})
-    public void newUser(WinterUser entity) {
+    @Produces(MediaType.TEXT_PLAIN)
+    public boolean newUser(WinterUser entity) {
         if (entity == null){
-            return;
+            return false;
         }
         EntityManager em = DefaultEntityManagerFactory.newDefaultEntityManager();
         em.getTransaction().begin();
         em.persist(entity);
         em.getTransaction().commit();
         em.close();
+        return true;
     }
     
     
