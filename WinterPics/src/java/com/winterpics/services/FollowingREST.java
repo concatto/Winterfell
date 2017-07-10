a4911f90f07eba0e20c4df7866f6396c03a1214c
Vinícius Almeida dos Santos <vinicius@rudinei.cnt.br>
2017-07-09 23:33:32 -0300

    Adicionado meio de obter quem outras pessoas estão seguinod

--- WinterPics/src/java/com/winterpics/services/FollowingREST.java
+++ WinterPics/src/java/com/winterpics/services/FollowingREST.java
@@ -10,6 +10,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
@@ -21,9 +22,19 @@ public class FollowingREST {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<WinterUser> getFeed(@Context HttpServletRequest request){
+    public List<WinterUser> getFollowing(@Context HttpServletRequest request){
         WinterUser user = (WinterUser) request.getAttribute("winteruser");
         return user.getFollowing();
+    }
+    
+    @GET
+    @Path("{otherID}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<WinterUser> getOtherFollowing(@PathParam("otherID") long otherID){
+        EntityManager em = DefaultEntityManagerFactory.newDefaultEntityManager();
+        return em.createQuery("SELECT u.following FROM WinterUser u WHERE u.id=:id")
+            .setParameter("id", otherID)
+            .getResultList();
     }
     
     private Response updateFolowing(WinterUser user){
